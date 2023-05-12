a9f91926432ea44e1b9338e4351d43f61e3dcd1d
RubenRT <32521403+rubenaz20@users.noreply.github.com>
2023-05-12 12:54:08 +0200

    Update and rename zoom_record_template1.py to zoom_script.py
    Added sign in into Zoom , removed recording

--- zoom_script.py
+++ zoom_script.py
@@ -18,20 +18,16 @@
 
 ########################################################
 
-## Created by Rohit Kannachel
-## Not liable if this causes any issues.
-## thank you to Tanushi De Silva for giving me the inspiration to make this
-
 # importing libraries
 import pyautogui 
 import time
-
 
 #####   Joining Zoom Meeting   ###################
 
 #######################################################################################
 #Enter the meeting id as a string here *important that it is in string format
 meet_id = '123456789'
+
 #esc clicked to ensure that the win key will open up correctly in the next step
 pyautogui.press('esc',interval=0.1)
 
@@ -45,6 +41,48 @@ pyautogui.press('enter',interval=0.5)
 
 #time delay to factor for zoom app to load up, good buffer is like 10 sec but its case specific
 time.sleep(10)
+
+
+#####   SignIn into Zoom   ###################
+
+user = '123456789'
+user_pwd = '123456789'
+
+# Check is loggedIn and signout
+loggedIn = pyautogui.locateOnScreen('loggedIn.png')
+
+if loggedIn {
+  # Click on greenBullet
+  greenBullet = pyautogui.locateOnScreen('loggedIn_green_bullet.png')
+  center_greenBullet = pyautogui.center(greenBullet)
+  pyautogui.click(center_greenBullet)
+  
+  # Click on signout
+  logOut = pyautogui.locateOnScreen('logOut.png')
+  center_logOut = pyautogui.center(logOut)
+  pyautogui.click(center_logOut)
+  
+  time.sleep(4)
+}
+
+# Search user field by placeholder
+user_box = pyautogui.locateOnScreen('username_placeholder.png')
+center_username = pyautogui.center(user_box)
+pyautogui.click(center_username)
+pyautogui.typewrite(user)
+
+# Search user pwd field by placeholder
+pwd_box = pyautogui.locateOnScreen('user_pwd_placeholder.png')
+center_pwd = pyautogui.center(pwd_box)
+pyautogui.click(center_pwd)
+pyautogui.typewrite(user_pwd)
+
+# Search signin button
+signinButton = pyautogui.locateOnScreen('signinButton.png')
+center_signinButton = pyautogui.center(signinButton)
+pyautogui.click(center_signinButton)
+
+time.sleep(8)
 
 
 #this part simulates clicking join meeting, entering meeting id and pressing enter to join
@@ -73,50 +111,3 @@ password = 'password'
 #pyautogui.write(password)
 #pyautogui.press('enter',interval = 1)
 ##### IF YOUR MEETING HAS NO PASSWORD PLEASE LEAVE THIS SECTION AS IS AND DONT UNCOMMENT
-
-
-
-##################################################################################
-
-
-###### Recording meeting using Windows Game Bar  #############################
-
-
-#######################################################################################
-
-
-## this time buffer is added so that it accounts for the time taken to load into the meeting 
-## a good buffer time is around 10-20 seconds before recording starts to ensure you're in the meeting
-
-
-time.sleep(5)
-
-
-## opening up windows game bar overlay
-pyautogui.hotkey('win','g')
-time.sleep(1)
-## commencing screen recording
-pyautogui.hotkey('win','alt','r')
-time.sleep(1)
-## closing windows game bar overlay
-pyautogui.hotkey('win','g')
-
-
-#### recording time amount
-## however long you want, enter the time here in seconds, e.g. 30 minutes is 60*30 = 1800 seconds
-## in windows game bar the default setting for time limit for recording is 2 hours,
-## make sure to change this as you need
-time.sleep(25)
-
-
-## ending screen recording
-pyautogui.hotkey('win','alt','r')
-time.sleep(2)
-## By default, screen captures are sent to a folder called captures in "videos" in "this PC"
-
-## closing Zoom
-pyautogui.hotkey('alt','f4')
-time.sleep(0.5)
-pyautogui.hotkey('alt','f4')
-
-############################################################################################
